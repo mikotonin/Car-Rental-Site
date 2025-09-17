e6875f75dd0e43e7bfaf0d9ee4500ad13f02952c
Mike Lacsina <mikelacsina21@gmail.com>
2025-09-17 19:19:43 +0800

    updated javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+function openMenu() {
+  const body = document.body;
+
+  body.classList.add("open");
+}
+
+function closeMenu() {
+  const body = document.body;
+
+  body.classList.remove("open");
+}
